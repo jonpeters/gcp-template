839948e8ae4411a4dc541b26f3157fbc9da0f6a9
Jon Peters <jpeters3047@gmail.com>
2023-03-27 19:46:51 -0400

    try enabling api again

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,12 +52,13 @@ resource "google_project_service" "cloud_run_api" {
   }
 }
 
-resource "google_project_service" "cloud_resource_manager_run_api" {
+resource "google_project_service" "cloud_resource_manager_api" {
   project = var.project_id
   service = "cloudresourcemanager.googleapis.com"
 
   timeouts {
-    create = "5m"
+    create = "10m"
+    update = "10m"
   }
 }
 
